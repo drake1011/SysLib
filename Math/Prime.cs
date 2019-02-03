4acc020e5e12611f25604ec720484273cfbce34f
drake-vcu <dkaempfer46@gmail.com>
2019-02-02 18:12:33 -0600

    Add Math.Prime and tests

---
+++ Math/Prime.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Math
+{
+    public static class Prime
+    {
+        private const int _maxPrime = 2146435069;
+
+        /// <summary>
+        /// Checks if number is a prime number
+        /// </summary>
+        /// <param name="num">input</param>
+        public static bool IsPrime(int num)
+        {
+            if ((num & 1) != 0)
+            {
+                for (int i = 2; i < num; i++)
+                {
+                    if (num % i == 0)
+                        return false;
+                }
+                return true;
+            }
+            return (num == 2);
+        }
+
+        /// <summary>
+        /// Return the next prime number after the input
+        /// </summary>
+        /// <param name="num">seed</param>
+        public static int NextPrime(int num)
+        {
+            do
+            {
+                if (IsPrime(++num))
+                    return num;
+            } while (num < _maxPrime);
+            return num;
+        }
+
+        /// <summary>
+        /// Return the first prime number that came before the input
+        /// </summary>
+        /// <param name="num">seed</param>
+        public static int PreviousPrime(int num)
+        {
+            do
+            {
+                if (IsPrime(--num))
+                    return num;
+            } while (num > int.MinValue);
+            return num;
+        }
+    }
+}
---
+++ XUnitTests/PrimeTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Xunit.Extensions;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace XUnitTests
+{
+    public class PrimeTests
+    {
+        [Theory]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(199)]
+        [InlineData(1999993)]
+        [InlineData(1342193473)]
+        [InlineData(2146435069)]
+        public void Test_IsPrime_True_Pass(int input)
+        {
+            Assert.True(Math.Prime.IsPrime(input));
+        }
+
+        [Theory]
+        [InlineData(4)]
+        [InlineData(6)]
+        [InlineData(8)]
+        [InlineData(1999999)]
+        [InlineData(2146435070)]
+        public void Test_IsPrime_False_Pass(int input)
+        {
+            Assert.False(Math.Prime.IsPrime(input));
+        }
+
+        [Theory]
+        [InlineData(2, 3)]
+        [InlineData(3, 5)]
+        [InlineData(4, 5)]
+        [InlineData(5564, 5569)]
+        [InlineData(12158, 12161)]
+        [InlineData(65323, 65327)]
+        [InlineData(65324, 65327)]
+        public void Test_NextPrime_Pass(int input, int expected)
+        {
+            Assert.Equal(expected, Math.Prime.NextPrime(input));
+        }
+
+        [Theory]
+        [InlineData(4, 3)]
+        [InlineData(6, 5)]
+        [InlineData(7, 5)]
+        [InlineData(5569, 5563)]
+        [InlineData(12161, 12157)]
+        [InlineData(65327, 65323)]
+        [InlineData(65326, 65323)]
+        public void Test_PreviousPrime_Pass(int input, int expected)
+        {
+            Assert.Equal(expected, Math.Prime.PreviousPrime(input));
+        }
+    }
+}
